62e3da591c303943c1cc3e5375395f537f9b95de
Seung Hwan (David) Lee <50871680+gitmshdl@users.noreply.github.com>
2021-11-14 01:59:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -28,12 +28,7 @@ const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL;
 
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
